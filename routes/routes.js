dda803c45f5aee44cd3b33b671ba90bf676dd60b
Viccenzo <viccenzobarbosa86@gmail.com>
2025-05-09 12:28:03 -0300

    fixroutes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
-const modeloTarefa = require("../models/tarefasDB");
+const modeloTarefa = require("../models/tarefa");
 
 router.post("/post", async (req, res) => {
   const objetoTarefa = new modeloTarefa({
