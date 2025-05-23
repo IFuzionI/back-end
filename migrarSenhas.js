89af80fa16f466249b2cf0b15d3924a3bb087841
unisanta <unisanta@santacecilia.br>
2025-05-23 20:46:57 -0300

    fix crypto

--- migrarSenhas.js
+++ migrarSenhas.js
@@ -1,40 +1,25 @@
 const mongoose = require('mongoose');
-const { randomBytes, createHash } = require('crypto');
-const userModel = require('./models/user.js'); // ajuste o caminho conforme necessário
+const { createHash, randomBytes } = require('crypto');
+const userModel = require('./models/user');
 
-mongoose.connect('mongodb+srv://viccenzo243515:tenera2025@cluster0.efkttba.mongodb.net/tarefasDB?retryWrites=true&w=majority&appName=Cluster0', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-}).then(() => {
-    console.log('Conectado ao MongoDB');
-    migrarUsuarios();
-}).catch(err => {
-    console.error('Erro ao conectar no MongoDB:', err);
-});
+mongoose.connect('mongodb+srv://viccenzo243515:tenera2025@cluster0.efkttba.mongodb.net/tarefasDB?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function migrarUsuarios() {
-    try {
-        const usuarios = await userModel.find({ senha: { $exists: true } });
+async function migratePasswords() {
+  const users = await userModel.find();
 
-        console.log(`Encontrado(s) ${usuarios.length} usuário(s) para migrar.`);
+  for (let user of users) {
+    const salt = randomBytes(16).toString('hex');
+    const hash = createHash('sha256').update('1234' + salt).digest('hex'); // Aqui você coloca a senha padrão ou a atual de cada usuário
 
-        for (const user of usuarios) {
-            const salt = randomBytes(16).toString('hex');
-            const hash = createHash('sha256').update(user.senha + salt).digest('hex');
+    user.hash = hash;
+    user.salt = salt;
 
-            user.hash = hash;
-            user.salt = salt;
-            user.senha = undefined; // Remove o campo antigo
+    await user.save();
 
-            await user.save();
+    console.log(`Usuário ${user.nome} migrado com sucesso.`);
+  }
 
-            console.log(`Usuário ${user.nome} migrado com sucesso.`);
-        }
-
-        console.log('Migração concluída.');
-    } catch (err) {
-        console.error('Erro durante a migração:', err);
-    } finally {
-        mongoose.disconnect();
-    }
+  mongoose.connection.close();
 }
+
+migratePasswords().catch(err => console.error(err));
